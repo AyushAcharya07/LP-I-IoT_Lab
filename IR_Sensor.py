0890af5a0607aec2998a6670263d337d7f3b47bc
Ayush_S_Acharya <115517887+AyushAcharya07@users.noreply.github.com>
2023-09-30 21:49:14 +0530

    Create IR_Sensor.py

---
+++ IR_Sensor.py
@@ -0,0 +1,18 @@
+import Rpi.GPIO as g
+import time
+
+g.setmode(g.BOARD)
+
+g.setup(12,g.IN)
+g.setup(22,g.OUT)
+
+while True:
+  var=g.input(12)
+  print(var)
+  if var==0:
+    g.output(22,True)
+    time.sleep(5)
+  else:
+    g.output(22,False)
+
+g.cleanup()
